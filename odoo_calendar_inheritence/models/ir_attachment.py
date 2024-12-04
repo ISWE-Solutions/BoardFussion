083bd190234215731e9a3e1416692afb6b98ff81
Thom Simbeye <thomsimbeye99@gmail.com>
2024-12-04 10:59:59 +0200

    removed zips

--- odoo_calendar_inheritence/models/ir_attachment.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, api, models, _
-
-class Ir_Attachment(models.Model):
-    _inherit = 'ir.attachment'
-
-    def unlink(self):
-        # print("Executed!")
-        res = super(Ir_Attachment, self).unlink()
-        return res
--- odoo_calendar_inheritence/static/src/js/action_board_knowledge.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.action-btn').forEach(button => {
-    button.addEventListener('click', function () {
-        console.log('Button clicked');
-    });
-});
